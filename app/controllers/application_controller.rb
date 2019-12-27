1389101b3868609e3548b7d7cf6ed66a0c673e52
afzalMA <afzal.m@etvbharat.com>
2019-12-27 11:54:06 +0530

    added some modifications to test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     def hello
-        render html: "hello, world"
+        render html: "hello world, rails welcomes you!"
     end
 end
